c055ac28aaffaa25b2fc4c724bb1a2760a3d62b1
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-04-19 13:14:03 -0400

    Cleanup
    Restore balance in the languages bar.

--- grand-theft-mario/actors/magnum/geo_header.h
+++
@@ -1,8 +0,0 @@
-extern const GeoLayout magnum_geo[];
-extern Lights1 magnum_Material_f3d_lights;
-extern u8 magnum_magnumtex_rgba16[];
-extern Vtx magnum_gun_mesh_layer_1_vtx_0[743];
-extern Gfx magnum_gun_mesh_layer_1_tri_0[];
-extern Gfx mat_magnum_Material_f3d[];
-extern Gfx magnum_gun_mesh_layer_1[];
-extern Gfx magnum_material_revert_render_settings[];
