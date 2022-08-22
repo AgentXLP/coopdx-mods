04bc03225aabefd5740dc0daefcc674ddaa6d655
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-08-22 13:07:57 -0400

    Climb the Tower v1.2

--- mods/climb-the-tower/main.lua
+++ mods/climb-the-tower/main.lua
@@ -1,5 +1,6 @@
 -- name: Climb the Tower
--- description: Climb the Tower!\nBy: \\#ff7f00\\Agent X\\#ffffff\\\n\nThis gamemode revolves around getting the fastest time and getting to the top of the tower the fastest, you can view the scores with /scoreboard\nIf you wish to play another gamemode on this map (e.g. manhunt / hide and seek) then run /ctt off on host.
+-- incompatible: gamemode
+-- description: Climb the Tower v1.2\nBy: \\#ec7731\\Agent X\\#ffffff\\\n\nThis gamemode revolves around getting the fastest time and getting to the top of the tower the fastest, you can view the scores with /scoreboard\nIf you wish to play another gamemode on this map (e.g. manhunt / hide and seek) then run /ctt off on host.
 
 gGlobalSyncTable.gameEnabled = true
 doubleJumps = 3
@@ -8,6 +9,7 @@ function in_range(val, low, max)
     return val >= low and val <= max
 end
 
+--- @param m MarioState
 function disable_fall_damage(m)
     m.peakHeight = m.pos.y
 end
@@ -21,36 +23,36 @@ end
 
 --- @param m MarioState
 function mario_update(m)
+    if m.playerIndex ~= 0 then return end
+
     disable_fall_damage(m)
 
-    if m.playerIndex == 0 then
-        if gPlayerSyncTable[0].finished == false then
-            gPlayerSyncTable[0].time = gPlayerSyncTable[0].time + 1
-        end
-        if gNetworkPlayers[0].currLevelNum ~= LEVEL_PSS then
-            warp_to_level(LEVEL_PSS, 0x01, 1)
-            gPlayerSyncTable[0].time = 0
-            gPlayerSyncTable[0].finished = false
-        end
+    if gPlayerSyncTable[0].finished == false then
+        gPlayerSyncTable[0].time = gPlayerSyncTable[0].time + 1
+    end
+    if gNetworkPlayers[0].currLevelNum ~= LEVEL_PSS then
+        warp_to_level(LEVEL_PSS, 0x01, 1)
+        gPlayerSyncTable[0].time = 0
+        gPlayerSyncTable[0].finished = false
+    end
 
-        if gGlobalSyncTable.gameEnabled == false then return end
+    if gGlobalSyncTable.gameEnabled == false then return end
 
-        if m.pos.y >= 10889 then
-            gPlayerSyncTable[0].finished = true
-        end
+    if m.pos.y >= 10889 and (m.action & ACT_FLAG_AIR) == 0 then
+        gPlayerSyncTable[0].finished = true
+    end
 
-        if (m.controller.buttonPressed & D_JPAD) ~= 0 then
-            on_reset_command()
-        end
+    if (m.controller.buttonPressed & D_JPAD) ~= 0 then
+        on_reset_command()
+    end
 
-        -- special double jump
-        if (m.action & ACT_GROUP_MASK) == ACT_GROUP_AIRBORNE and (m.controller.buttonPressed & Y_BUTTON) ~= 0 then
-            if doubleJumps ~= 0 then
-                doubleJumps = doubleJumps - 1
-                set_mario_action(m, ACT_LONG_JUMP, 0)
-                m.faceAngle.y = m.intendedYaw
-                m.vel.y = 50
-            end
+    -- special double jump
+    if (m.action & ACT_GROUP_MASK) == ACT_GROUP_AIRBORNE and (m.controller.buttonPressed & Y_BUTTON) ~= 0 then
+        if doubleJumps ~= 0 then
+            doubleJumps = doubleJumps - 1
+            set_mario_action(m, ACT_LONG_JUMP, 0)
+            m.faceAngle.y = m.intendedYaw
+            m.vel.y = 50
         end
     end
 end
@@ -64,7 +66,7 @@ end
 
 function on_hud_render()
     if gGlobalSyncTable.gameEnabled == false then return end
-    -- render to N64 screen space, with the HUD font
+
     djui_hud_set_resolution(RESOLUTION_N64)
     djui_hud_set_font(FONT_NORMAL)
 
@@ -73,14 +75,12 @@ function on_hud_render()
         text = tostring(string.format("%.3f", gPlayerSyncTable[0].time / 30)) .. " seconds"
     else return end
 
-    -- get width of screen and text
     local screenWidth = djui_hud_get_screen_width()
     local width = djui_hud_measure_text(text) * 0.50
 
     local x = (screenWidth - width) / 2.0
     local y = 0
 
-    -- render top
     djui_hud_set_color(0, 0, 0, 128)
     djui_hud_render_rect(x - 6, y, width + 12, 16)
     if gPlayerSyncTable[0].time < 2700 then
@@ -100,11 +100,6 @@ function on_hud_render()
 end
 
 function on_ctt_command(msg)
-    if not network_is_server() then
-        djui_chat_message_create("You need to be the host to run this command!")
-        return false
-    end
-
     if msg == "on" then
         gGlobalSyncTable.gameEnabled = true
         djui_chat_message_create("CTT enabled! Based.")
@@ -119,7 +114,6 @@ end
 
 function on_reset_command()
     warp_to_level(LEVEL_PSS, 0x01, 1)
-    on_warp()
     return true
 end
 
@@ -143,6 +137,8 @@ function on_finished_change(tag, oldVal, newVal)
     end
 end
 
+gLevelValues.entryLevel = LEVEL_PSS
+gLevelValues.fixCollisionBugs = 1
 hud_hide()
 smlua_text_utils_secret_star_replace(COURSE_PSS,      "   CLIMB THE TOWER")
 smlua_text_utils_dialog_replace(DIALOG_149, 1, 4, 30, 200, "Welcome to Climb the Tower\
@@ -161,8 +157,11 @@ hook_event(HOOK_ON_WARP, on_warp)
 hook_event(HOOK_MARIO_UPDATE, mario_update)
 hook_event(HOOK_ON_PLAYER_CONNECTED, on_player_connected)
 hook_event(HOOK_ON_HUD_RENDER, on_hud_render)
+hook_event(HOOK_ON_PAUSE_EXIT, function() return false end)
 
-hook_chat_command("ctt", "[on|off] modify whether or not the gamemode is enabled", on_ctt_command)
+if network_is_server() then
+    hook_chat_command("ctt", "[on|off] modify whether or not the gamemode is enabled", on_ctt_command)
+end
 hook_chat_command("reset", "to reset the level for everybody", on_reset_command)
 hook_chat_command("scoreboard", "not in any paticular order because sorting stuff is hard", on_scoreboard_command)
 
--- mods/climb-the-tower/yoshi.lua
+++ mods/climb-the-tower/yoshi.lua
@@ -1,5 +1,6 @@
 triggered = false
 
+--- @param o Object
 function bhv_stationary_yoshi_init(o)
     o.oFlags = OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE
     o.oInteractType = INTERACT_IGLOO_BARRIER
@@ -12,13 +13,16 @@ function bhv_stationary_yoshi_init(o)
     o.hitboxHeight = 150
 end
 
+--- @param o Object
 function bhv_stationary_yoshi_loop(o)
     cur_obj_init_animation(0)
-    if dist_between_objects(o, gMarioStates[0].marioObj) < 500 and triggered == false then
+    if dist_between_objects(o, gMarioStates[0].marioObj) < 300 and triggered == false then
         triggered = true
         play_puzzle_jingle()
-        doubleJumps = 3
+        doubleJumps = doubleJumps + 2
     end
+
+    o.header.gfx.angle.y = nearest_mario_state_to_object(o).faceAngle.y + 0x8000
 end
 
 id_bhvStationaryYoshi = hook_behavior(nil, OBJ_LIST_GENACTOR, true, bhv_stationary_yoshi_init, bhv_stationary_yoshi_loop)
