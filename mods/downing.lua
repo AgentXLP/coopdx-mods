42be22f1a1ba81e6b877a8f4353cbc907b14b2b0
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-10-22 23:54:46 -0400

    Downing update

--- mods/downing.lua
+++ mods/downing.lua
@@ -5,13 +5,14 @@ _G.downHealth = {}
 for i = 0, (MAX_PLAYERS - 1) do
     downHealth[i] = 300
 end
+
 gGlobalSyncTable.downing = true
 gGlobalSyncTable.customFallDamage = false
 
 PACKET_REVIVE = 0
 PACKET_POPUP = 1
 
-DOWNING_MIN_PLAYERS = 2
+DOWNING_MIN_PLAYERS = 1
 
 gotUp = false
 
@@ -33,28 +34,43 @@ function clamp(x, a, b)
     return x
 end
 
+function approach_number(current, target, inc, dec)
+    if current < target then
+        current = current + inc
+        if current > target then
+            current = target
+        end
+    else
+        current = current - dec
+        if current < target then
+            current = target
+        end
+    end
+    return current
+end
+
 function lerp(a,b,t) return a * (1-t) + b * t end
 
 --- @param m MarioState
 function active_player(m)
     local np = gNetworkPlayers[m.playerIndex]
     if m.playerIndex == 0 then
-        return true
+        return 1
     end
     if not np.connected then
-        return false
+        return 0
     end
     if np.currCourseNum ~= gNetworkPlayers[0].currCourseNum then
-        return false
+        return 0
     end
     if np.currActNum ~= gNetworkPlayers[0].currActNum then
-        return false
+        return 0
     end
     if np.currLevelNum ~= gNetworkPlayers[0].currLevelNum then
-        return false
+        return 0
     end
     if np.currAreaIndex ~= gNetworkPlayers[0].currAreaIndex then
-        return false
+        return 0
     end
     return is_player_active(m)
 end
@@ -87,7 +103,7 @@ function name_without_hex(name)
     return table.concat(nameTable, "")
 end
 
-l4dHud = check_for_mod("Left 4 Dead 2 HUD", false)
+l4d2Hud = check_for_mod("Left 4 Dead 2 HUD", false)
 
 --- @param m MarioState
 function update_fvel(m)
@@ -114,7 +130,7 @@ end
 
 --- @param m MarioState
 function undown(m)
-    m.health = 0x380
+    m.healCounter = 8
     set_mario_action(m, ACT_IDLE, 0)
     m.invincTimer = 60
     gPlayerSyncTable[m.playerIndex].downHealth = 300
@@ -122,8 +138,17 @@ function undown(m)
 end
 
 --- @param m MarioState
+function kill_downed(m)
+    gPlayerSyncTable[m.playerIndex].downHealth = 300
+    m.health = 0xff
+    set_mario_action(m, ACT_IDLE, 0)
+end
+
+--- @param m MarioState
 function act_down(m)
-    if network_player_connected_count() < DOWNING_MIN_PLAYERS then undown(m) end
+    if player_alive_count() < DOWNING_MIN_PLAYERS then
+        kill_downed(m)
+    end
 
     m.actionTimer = m.actionTimer + 1
 
@@ -139,8 +164,7 @@ function act_down(m)
         if gPlayerSyncTable[0].downHealth > 0 then
             m.health = 0x180
         else
-            gPlayerSyncTable[0].downHealth = 0
-            m.health = 0xff
+            kill_downed(m)
         end
 
         if m.actionTimer % 30 == 0 then gPlayerSyncTable[0].downHealth = gPlayerSyncTable[0].downHealth - 1 end
@@ -148,10 +172,27 @@ function act_down(m)
             gPlayerSyncTable[0].downHealth = gPlayerSyncTable[0].downHealth - m.hurtCounter * 4
             m.hurtCounter = 0
             m.invincTimer = 60
+            play_character_sound(m, CHAR_SOUND_ATTACKED)
         end
+
+        if (m.controller.buttonPressed & X_BUTTON) ~= 0 then kill_downed(m) end
     end
 end
-_G.ACT_DOWN = allocate_mario_action(ACT_FLAG_SHORT_HITBOX | ACT_FLAG_STATIONARY)
+_G.ACT_DOWN = allocate_mario_action(ACT_GROUP_AIRBORNE | ACT_FLAG_SHORT_HITBOX)
+
+--- @param m MarioState
+function is_hazard_floor(m)
+    return m.floor.type == SURFACE_BURNING
+    or m.floor.type == SURFACE_QUICKSAND
+    or m.floor.type == SURFACE_DEEP_QUICKSAND
+    or m.floor.type == SURFACE_MOVING_QUICKSAND
+    or m.floor.type == SURFACE_INSTANT_QUICKSAND
+    or m.floor.type == SURFACE_SHALLOW_QUICKSAND
+    or m.floor.type == SURFACE_DEEP_MOVING_QUICKSAND
+    or m.floor.type == SURFACE_INSTANT_MOVING_QUICKSAND
+    or m.floor.type == SURFACE_SHALLOW_MOVING_QUICKSAND
+    or m.floor.type == SURFACE_DEATH_PLANE
+end
 
 --- @param m MarioState
 function should_be_downed(m)
@@ -163,17 +204,7 @@ function should_be_downed(m)
     and m.action ~= ACT_DEATH_EXIT
     and m.action ~= ACT_DEATH_EXIT_LAND
     and (m.action & ACT_FLAG_INTANGIBLE) == 0
-    and m.floor.type ~= SURFACE_BURNING
-    and m.floor.type ~= SURFACE_QUICKSAND
-    and m.floor.type ~= SURFACE_DEEP_QUICKSAND
-    and m.floor.type ~= SURFACE_MOVING_QUICKSAND
-    and m.floor.type ~= SURFACE_INSTANT_QUICKSAND
-    and m.floor.type ~= SURFACE_SHALLOW_QUICKSAND
-    and m.floor.type ~= SURFACE_DEEP_MOVING_QUICKSAND
-    and m.floor.type ~= SURFACE_INSTANT_MOVING_QUICKSAND
-    and m.floor.type ~= SURFACE_SHALLOW_MOVING_QUICKSAND
-    and m.floor.type ~= SURFACE_DEATH_PLANE
-    and m.floor.type ~= SURFACE_VERTICAL_WIND
+    and not is_hazard_floor(m)
 end
 
 function get_fall_damage_multiplier(vel)
@@ -183,6 +214,14 @@ function get_fall_damage_multiplier(vel)
     elseif vel >= 130 then return 20 end
 
     return 20
+end
+
+function player_alive_count()
+    local count = 0
+    for i = 0, (MAX_PLAYERS - 1) do
+        if active_player(gMarioStates[i]) ~= 0 then count = count + 1 end
+    end
+    return count
 end
 
 extraVel = 0
@@ -197,7 +236,7 @@ function mario_update(m)
 
     if m.action == _G.ACT_DOWN then network_player_set_description(gNetworkPlayers[0], "Down", 255, 0, 0, 255) else network_player_set_description(gNetworkPlayers[0], "", 255, 255, 255, 255) end
 
-    if m.playerIndex ~= 0 or network_player_connected_count() < DOWNING_MIN_PLAYERS then return end
+    if m.playerIndex ~= 0 or player_alive_count() < DOWNING_MIN_PLAYERS then return end
 
     if (m.action & ACT_GROUP_MASK) ~= ACT_GROUP_CUTSCENE then gotUp = true end
 
@@ -232,7 +271,7 @@ end
 
 --- @param m MarioState
 function on_set_mario_action(m)
-    if m.playerIndex ~= 0 or network_player_connected_count() < DOWNING_MIN_PLAYERS then return end
+    if m.playerIndex ~= 0 or player_alive_count() < DOWNING_MIN_PLAYERS then return end
 
     if (m.prevAction == _G.ACT_DOWN or should_be_downed(m)) and (m.action & ACT_GROUP_MASK) == ACT_GROUP_SUBMERGED then
         m.health = 0xff
@@ -255,7 +294,7 @@ function on_hud_render()
     local height = djui_hud_get_screen_height()
 
     local m = gMarioStates[0]
-    if m.action == _G.ACT_DOWN and not l4dHud then
+    if m.action == _G.ACT_DOWN and not l4d2Hud then
         djui_hud_set_resolution(RESOLUTION_N64)
         djui_hud_set_font(FONT_HUD)
         djui_hud_set_adjusted_color(255, 255, 255, 255)
@@ -263,12 +302,12 @@ function on_hud_render()
     end
 
     local near = nearest_mario_state_to_object(m.marioObj)
-    if near ~= nil and dist_between_objects(m.marioObj, near.marioObj) < 250 and near.action == _G.ACT_DOWN and m.action ~= _G.ACT_DOWN and (m.action & ACT_GROUP_MASK) ~= ACT_GROUP_CUTSCENE then
+    if near ~= nil and active_player(near) ~= 0 and dist_between_objects(m.marioObj, near.marioObj) < 250 and near.action == _G.ACT_DOWN and m.action ~= _G.ACT_DOWN and (m.action & ACT_GROUP_MASK) ~= ACT_GROUP_CUTSCENE then
         if not soundPlayed then
             play_sound(SOUND_MENU_CHANGE_SELECT, m.marioObj.header.gfx.cameraToObject)
             soundPlayed = true
         end
-        if not (l4dHud and (m.controller.buttonDown & Z_TRIG) ~= 0) then
+        if not (l4d2Hud and (m.controller.buttonDown & Z_TRIG) ~= 0) then
             local text = "Help " .. name_without_hex(gNetworkPlayers[near.playerIndex].name) .. " up"
             local out = { x = 0, y = 0, z = 0 }
             djui_hud_world_pos_to_screen_pos(near.pos, out)
@@ -282,7 +321,7 @@ function on_hud_render()
         if (m.controller.buttonDown & Z_TRIG) ~= 0 then
             if reviveTimer > 0 then
                 reviveTimer = reviveTimer - 1
-                if l4dHud then
+                if l4d2Hud then
                     width = (djui_hud_get_screen_width() * 0.5) + 3
                     height = djui_hud_get_screen_height() * 0.5
                     djui_hud_set_adjusted_color(77, 73, 79, 255)
@@ -307,7 +346,7 @@ function on_hud_render()
                 end
             else
                 reviveTimer = reviveTime
-                network_send(true, { id = PACKET_REVIVE, global = network_global_index_from_local(near.playerIndex) })
+                network_send(true, { id = PACKET_REVIVE, global = network_global_index_from_local(near.playerIndex), savior = gNetworkPlayers[0].globalIndex })
             end
         else
             reviveTimer = reviveTime
@@ -326,7 +365,10 @@ function on_packet_receive(table)
     local m = gMarioStates[0]
 
     if table.id == PACKET_REVIVE then
-        if network_global_index_from_local(m.playerIndex) == table.global then undown(m) end
+        if network_global_index_from_local(m.playerIndex) == table.global then
+            undown(m)
+            vec3f_copy(m.pos, gMarioStates[network_local_index_from_global(table.savior)].pos)
+        end
     elseif table.id == PACKET_POPUP then
         djui_popup_create(table.msg, 1)
     end
@@ -352,7 +394,7 @@ function on_custom_fall_damage_command(msg)
         djui_chat_message_create("Custom fall damage status: \\#00ff00\\ON")
     else
         gGlobalSyncTable.customFallDamage = false
-        djui_chat_message_create("Custom fall damage status: \\#FF0000\\OFF")
+        djui_chat_message_create("Custom fall damage status: \\#ff0000\\OFF")
 
     end
     return true
