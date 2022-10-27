949981c7118f62b1e08faf101b95ef3ac3b718b7
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-10-26 21:33:43 -0400

    CTT 1.3.0.1

--- mods/climb-the-tower/main.lua
+++ mods/climb-the-tower/main.lua
@@ -1,8 +1,10 @@
 -- name: Climb the Tower
--- incompatible: gamemode romhack
+-- incompatible: gamemode
 -- description: Climb the Tower v1.3\nBy: \\#ec7731\\Agent X\\#ffffff\\\n\nThis gamemode revolves around getting the fastest time and getting to the top of the tower the fastest, you can view the scores with /scoreboard\nIf you wish to play another gamemode on this map (e.g. manhunt / hide and seek) then run /ctt off on host.
 
-gGlobalSyncTable.gameEnabled = true
+LEVEL_CTT = LEVEL_PSS
+
+gGlobalSyncTable.climbTheTower = true
 doubleJumps = 3
 
 function in_range(val, low, max)
@@ -10,33 +12,20 @@ function in_range(val, low, max)
 end
 
 --- @param m MarioState
-function disable_fall_damage(m)
-    m.peakHeight = m.pos.y
-end
-
-function on_warp()
-    gPlayerSyncTable[0].time = 0
-    gPlayerSyncTable[0].finished = false
-    doubleJumps = 3
-    triggered = false
-end
-
---- @param m MarioState
 function mario_update(m)
-    if m.playerIndex ~= 0 then return end
+    if m.playerIndex ~= 0 or not gGlobalSyncTable.climbTheTower then return end
 
-    disable_fall_damage(m)
+    m.peakHeight = m.pos.y
 
-    if gPlayerSyncTable[0].finished == false then
+    if not gPlayerSyncTable[0].finished then
         gPlayerSyncTable[0].time = gPlayerSyncTable[0].time + 1
     end
-    if gNetworkPlayers[0].currLevelNum ~= LEVEL_PSS then
-        warp_to_level(LEVEL_PSS, 0x01, 1)
+
+    if gNetworkPlayers[0].currLevelNum ~= LEVEL_CTT then
+        warp_to_level(LEVEL_CTT, 1, 1)
         gPlayerSyncTable[0].time = 0
         gPlayerSyncTable[0].finished = false
     end
-
-    if gGlobalSyncTable.gameEnabled == false then return end
 
     if m.pos.y >= 10886 and (m.action & ACT_FLAG_AIR) == 0 then
         gPlayerSyncTable[0].finished = true
@@ -48,7 +37,7 @@ function mario_update(m)
 
     -- special double jump
     if (m.action & ACT_GROUP_MASK) == ACT_GROUP_AIRBORNE and (m.controller.buttonPressed & Y_BUTTON) ~= 0 then
-        if doubleJumps ~= 0 then
+        if doubleJumps > 0 then
             doubleJumps = doubleJumps - 1
             set_mario_action(m, ACT_LONG_JUMP, 0)
             m.faceAngle.y = m.intendedYaw
@@ -64,8 +53,15 @@ function on_player_connected(m)
     gPlayerSyncTable[m.playerIndex].finished = false
 end
 
+function on_warp()
+    gPlayerSyncTable[0].time = 0
+    gPlayerSyncTable[0].finished = false
+    doubleJumps = 3
+    triggered = false
+end
+
 function on_hud_render()
-    if gGlobalSyncTable.gameEnabled == false then return end
+    if not gGlobalSyncTable.climbTheTower then return end
 
     djui_hud_set_resolution(RESOLUTION_N64)
     djui_hud_set_font(FONT_NORMAL)
@@ -73,7 +69,9 @@ function on_hud_render()
     local text = "0.000 seconds"
     if gPlayerSyncTable[0].time ~= nil then
         text = tostring(string.format("%.3f", gPlayerSyncTable[0].time / 30)) .. " seconds"
-    else return end
+    else
+        return
+    end
 
     local screenWidth = djui_hud_get_screen_width()
     local width = djui_hud_measure_text(text) * 0.50
@@ -93,7 +91,7 @@ function on_hud_render()
     djui_hud_print_text(text, x, y, 0.50)
 
     djui_hud_set_color(255, 255, 255, 255)
-    hud_render_power_meter(gMarioStates[0].health, djui_hud_get_screen_width() - 58, 0, 60, 60)
+    hud_render_power_meter(gMarioStates[0].health, djui_hud_get_screen_width() - 62, 0, 64, 64)
 
     djui_hud_set_font(FONT_HUD)
     djui_hud_print_text(tostring(doubleJumps), 5, 5, 1)
@@ -102,11 +100,11 @@ end
 
 function on_ctt_command(msg)
     if msg == "on" then
-        gGlobalSyncTable.gameEnabled = true
+        gGlobalSyncTable.climbTheTower = true
         djui_chat_message_create("CTT enabled! Based.")
         hud_hide()
     else
-        gGlobalSyncTable.gameEnabled = false
+        gGlobalSyncTable.climbTheTower = false
         djui_chat_message_create("CTT disabled.")
         hud_show()
     end
@@ -114,14 +112,14 @@ function on_ctt_command(msg)
 end
 
 function on_reset_command()
-    warp_to_level(LEVEL_PSS, 0x01, 1)
+    warp_to_level(LEVEL_CTT, 1, 1)
     return true
 end
 
 function on_scoreboard_command()
-    djui_chat_message_create("Reminder that this is in no paticular order because I can't find out how to sort times and such correctly.")
+
     for i = 0, (MAX_PLAYERS - 1) do
-        if gNetworkPlayers[i].connected == true then
+        if gNetworkPlayers[i].connected then
             djui_chat_message_create(gNetworkPlayers[i].name .. "\\#ffffff\\ - " .. tostring(string.format("%.3f", gPlayerSyncTable[i].prevTime / 30)) .. " seconds")
         end
     end
@@ -129,8 +127,9 @@ function on_scoreboard_command()
 end
 
 function on_finished_change(tag, oldVal, newVal)
-    if gGlobalSyncTable.gameEnabled == false then return end
-    if newVal == true then
+    if not gGlobalSyncTable.climbTheTower then return end
+
+    if newVal then
         play_race_fanfare()
         gPlayerSyncTable[0].prevTime = gPlayerSyncTable[0].time
         djui_chat_message_create("Your time: " .. "\\#ffffff\\" .. tostring(string.format("%.3f", gPlayerSyncTable[0].prevTime / 30)) .. " seconds")
@@ -138,31 +137,20 @@ function on_finished_change(tag, oldVal, newVal)
     end
 end
 
-gLevelValues.entryLevel = LEVEL_PSS
-
 hud_hide()
 smlua_text_utils_secret_star_replace(COURSE_PSS,      "   CLIMB THE TOWER")
-smlua_text_utils_dialog_replace(DIALOG_149, 1, 4, 30, 200, "Welcome to Climb the Tower\
-This is a showcase of\
-sm64ex-coop unst 24's\
-new custom level system.\
-In the skies above Peach's\
-Castle, we came together\
-and built a big obstacle\
-course for the ones who\
-wish to take the\
-course and unlock their\
-true splendor.")
-hook_event(HOOK_ON_WARP, on_warp)
+
 hook_event(HOOK_MARIO_UPDATE, mario_update)
 hook_event(HOOK_ON_PLAYER_CONNECTED, on_player_connected)
+hook_event(HOOK_ON_WARP, on_warp)
 hook_event(HOOK_ON_HUD_RENDER, on_hud_render)
 hook_event(HOOK_ON_PAUSE_EXIT, function() return false end)
+
+hook_chat_command("reset", "to reset the level for everybody", on_reset_command)
+hook_chat_command("scoreboard", "not in any paticular order because sorting stuff is hard", on_scoreboard_command)
 
 if network_is_server() then
     hook_chat_command("ctt", "[on|off] modify whether or not the gamemode is enabled", on_ctt_command)
 end
-hook_chat_command("reset", "to reset the level for everybody", on_reset_command)
-hook_chat_command("scoreboard", "not in any paticular order because sorting stuff is hard", on_scoreboard_command)
 
 hook_on_sync_table_change(gPlayerSyncTable[0], "finished", 0, on_finished_change)
