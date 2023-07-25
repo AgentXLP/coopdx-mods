534c4d1f7279990eb40f90a931ab4160fe5dfd2f
Agent X <44549182+Agent-11@users.noreply.github.com>
2023-07-25 12:46:22 -0400

    Update Easy Ceiling Cling

--- mods/easy-ceiling-cling.lua
+++ mods/easy-ceiling-cling.lua
@@ -1,9 +1,11 @@
 -- name: Easy Ceiling Cling
 -- description: Easy Ceiling Cling\nBy \\#ec7731\\Agent X\\#dcdcdc\\\n\nThis mod adds improved ceiling hang detection system so you can now begin clinging to ceiling from actions outside\nof just the single and double jump.
 
+local set_mario_action = set_mario_action
+
 --- @param m MarioState
 local function before_phys_step(m)
-    if m.ceil ~= nil and m.ceil.type == SURFACE_HANGABLE and (m.action == ACT_TRIPLE_JUMP or m.action == ACT_WALL_KICK_AIR) then
+    if m.ceil ~= nil and m.ceil.type == SURFACE_HANGABLE and (m.action == ACT_TRIPLE_JUMP or m.action == ACT_WALL_KICK_AIR) and (m.input & INPUT_A_DOWN) ~= 0 then
         if m.pos.y + 180 > m.ceil.lowerY then
             set_mario_action(m, ACT_START_HANGING, 0)
         end
